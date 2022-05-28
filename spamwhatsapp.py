f26d6a32f238f239679fda956b98913fa9667db8
Srish Rachamalla <78019070+srishrachamalla7@users.noreply.github.com>
2022-05-28 16:28:13 +0530

    Add files via upload
    I created this program by using pywhatkit this is easy and can be done by any python beginner 
    You better use this on your friend and make your friend block you so you can have peaceful and boring life 😊

---
+++ spamwhatsapp.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "3d05592e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pyautogui as auto"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "b409c6ad",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from time import sleep"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "432dfa40",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "i=0\n",
+    "while i<100:\n",
+    "    auto.write('arey tupas yedava')\n",
+    "    auto.press('enter')\n",
+    "    sleep(1) "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
